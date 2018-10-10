35be0e4309de413d4992571014964d2e9347f0af
Felix Tran <tranfelix99@gmx.ch>
2018-10-10 10:23:10 +0200

    No random enemy spawn Pass putExtra data from PrologueFragment to Battleactivity to get Stage Correct enemy is shown depending on stage

--- app/src/main/java/ch/rpg/felix/rpg/BattleSystem/BattleActivity.java
+++ app/src/main/java/ch/rpg/felix/rpg/BattleSystem/BattleActivity.java
@@ -2,28 +2,40 @@ package ch.rpg.felix.rpg.BattleSystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import ch.rpg.felix.rpg.BattleSystem.Enemies;
 import ch.rpg.felix.rpg.BattleSystem.Player;
 import ch.rpg.felix.rpg.BattleSystem.Skills;
 import ch.rpg.felix.rpg.R;
+import ch.rpg.felix.rpg.WorldFragmentChildren.PrologueFragment;
 
 public class BattleActivity extends AppCompatActivity {
 
     Enemies enemies = new Enemies();
-    Player player = new Player();
-    Skills skills = new Skills();
 
-    //yet to be implemented
-    private int currentHp;
-
-    public void showEnemy() {
+    public void getEnemyStats() {
         TextView enemyName = (TextView) findViewById(R.id.enemy_name);
         TextView enemyHp = (TextView) findViewById(R.id.enemy_maxHp);
-
         enemyName.setText(enemies.getEnemy_name());
-        enemyHp.setText(currentHp + " / " + String.valueOf(enemies.getEnemy_hp()));
+        enemyHp.setText(String.valueOf(enemies.getEnemy_hp()));
+    }
+
+    public void showEnemy() {
+        int stage1 = Integer.parseInt(getIntent().getStringExtra("stage"));
+
+        if (stage1 == 11) {
+            enemies.Rat();
+            getEnemyStats();
+        } else if (stage1 == 12) {
+            enemies.Goblin();
+            getEnemyStats();
+        } else if (stage1 == 13) {
+            enemies.Ogre();
+            getEnemyStats();
+        }
+
     }
 
     @Override
--- app/src/main/java/ch/rpg/felix/rpg/WorldFragmentChildren/PrologueFragment.java
+++ app/src/main/java/ch/rpg/felix/rpg/WorldFragmentChildren/PrologueFragment.java
@@ -9,11 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import ch.rpg.felix.rpg.BattleSystem.BattleActivity;
+import ch.rpg.felix.rpg.BattleSystem.Enemies;
 import ch.rpg.felix.rpg.R;
 
 public class PrologueFragment extends Fragment {
+
+    private int stage;
 
     @Nullable
     @Override
@@ -23,7 +27,15 @@ public class PrologueFragment extends Fragment {
         return view;
     }
 
+    public void getIntoBattleactivity() {
+        Intent i = new Intent();
+        i.setClass(getActivity(), BattleActivity.class);
+        i.putExtra("stage", String.valueOf(stage));
+        getActivity().startActivity(i);
+    }
+
     private void battleBtn(View view){
+
         final Button btn1_1 = (Button) view.findViewById(R.id.btn_1_1);
         final Button btn1_2 = (Button) view.findViewById(R.id.btn_1_2);
         final Button btn1_3 = (Button) view.findViewById(R.id.btn_1_3);
@@ -31,9 +43,8 @@ public class PrologueFragment extends Fragment {
         btn1_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity(), BattleActivity.class);
-                getActivity().startActivity(intent);
+                stage = 11;
+                getIntoBattleactivity();
                 btn1_2.setVisibility(View.VISIBLE);
             }
         });
@@ -41,7 +52,18 @@ public class PrologueFragment extends Fragment {
         btn1_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                stage = 12;
+                getIntoBattleactivity();
                 btn1_3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn1_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stage = 13;
+                getIntoBattleactivity();
+                //btn1_4.setVisibility(View.VISIBLE);       fourth stage not implemented yet
             }
         });
     }
