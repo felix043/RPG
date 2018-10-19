367ea6f6c69f766acf22b9fd5c79dc69e546bdbf
Felix Tran <tranfelix99@gmx.ch>
2018-10-19 19:36:04 +0200

    Added "ownedItemsTable" to databasee

---
+++ app/src/main/java/ch/rpg/felix/rpg/Database/ItemOwnedAdapter.java
@@ -0,0 +1,58 @@
+package ch.rpg.felix.rpg.Database;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.rpg.felix.rpg.R;
+
+public class ItemOwnedAdapter extends RecyclerView.Adapter<ItemOwnedAdapter.ItemHolder> {
+
+    private List<ItemsOwnedEntity> itemsOwnedEntity = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_equipment, parent, false);
+        return new ItemHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        ItemsOwnedEntity currentItem = itemsOwnedEntity.get(position);
+        holder.itemname.setText(currentItem.getItemOwned_name());
+        holder.itemdescription.setText("+ " + String.valueOf(currentItem.getItemOwned_basedamage()) + " ATK");
+        holder.equipitem.setText("Equip");
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsOwnedEntity.size();
+    }
+
+    public void setOwnedNotes(List<ItemsOwnedEntity> itemsOwnedEntity) {
+        this.itemsOwnedEntity = itemsOwnedEntity;
+        notifyDataSetChanged();
+    }
+
+    public class ItemHolder extends RecyclerView.ViewHolder {
+        private TextView itemname;
+        private TextView itemdescription;
+        private Button equipitem;
+
+        public ItemHolder(View itemView) {
+            super(itemView);
+            itemname = itemView.findViewById(R.id.txt_itemname);
+            itemdescription = itemView.findViewById(R.id.txt_details);
+            equipitem = itemView.findViewById(R.id.btn_equip);
+        }
+    }
+}
---
+++ app/src/main/java/ch/rpg/felix/rpg/Database/ItemsOwnedEntity.java
@@ -0,0 +1,79 @@
+package ch.rpg.felix.rpg.Database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "ownedItemsTable")
+public class ItemsOwnedEntity {
+
+    @PrimaryKey
+    @ColumnInfo(name = "item_id")
+    private int idOwned;
+
+    private String itemOwned_name;
+    private String itemOwned_description;
+    private int itemOwned_basedamage;
+    private int itemOwned_damagemodifier;
+    private int itemOwned_category;
+    private int itemOwned_hpincrement;
+    private int itemOwned_costbronze;
+    private int itemOwned_costsilver;
+    private int itemOwned_costgold;
+
+    public ItemsOwnedEntity(int itemOwned_category, String itemOwned_name, String itemOwned_description, int itemOwned_basedamage, int itemOwned_damagemodifier, int itemOwned_hpincrement, int itemOwned_costbronze, int itemOwned_costsilver, int itemOwned_costgold) {
+        this.itemOwned_category = itemOwned_category;
+        this.itemOwned_name = itemOwned_name;
+        this.itemOwned_description = itemOwned_description;
+        this.itemOwned_basedamage = itemOwned_basedamage;
+        this.itemOwned_damagemodifier = itemOwned_damagemodifier;
+        this.itemOwned_hpincrement = itemOwned_hpincrement;
+        this.itemOwned_costbronze = itemOwned_costbronze;
+        this.itemOwned_costsilver = itemOwned_costsilver;
+        this.itemOwned_costgold = itemOwned_costgold;
+    }
+
+    public int getIdOwned() {
+        return idOwned;
+    }
+
+    public void setIdOwned(int IdOwned) {
+        this.idOwned = idOwned;
+    }
+
+    public int getItemOwned_category() {
+        return itemOwned_category;
+    }
+
+    public int getItemOwned_hpincrement() {
+        return itemOwned_hpincrement;
+    }
+
+    public String getItemOwned_name() {
+        return itemOwned_name;
+    }
+
+    public String getItemOwned_description() {
+        return itemOwned_description;
+    }
+
+    public int getItemOwned_basedamage() {
+        return itemOwned_basedamage;
+    }
+
+    public int getItemOwned_damagemodifier() {
+        return itemOwned_damagemodifier;
+    }
+
+    public int getItemOwned_costbronze() {
+        return itemOwned_costbronze;
+    }
+
+    public int getItemOwned_costsilver() {
+        return itemOwned_costsilver;
+    }
+
+    public int getItemOwned_costgold() {
+        return itemOwned_costgold;
+    }
+}
